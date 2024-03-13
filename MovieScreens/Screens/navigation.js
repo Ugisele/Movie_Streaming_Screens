904715164839ad93000775d17b5a68e224996db3
Ugisele <Umutonigisele12@gmail.com>
2024-03-13 09:50:10 +0200

    Stack nav Adds

--- MovieScreens/Screens/navigation.js
+++ MovieScreens/Screens/navigation.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Icon } from "react-native-elements";
